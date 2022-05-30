b68be5802766a4d96c8ff08e4a8c9342ee1ae83e
Bruce <contact@estimatebodyfat.com>
2022-05-29 22:34:12 -0400

    updated BMI calculator

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
 document.querySelector('#calculate-BMI').addEventListener('click', makeReq)
 
-async function makeReq(){
+async function makeReq() {
 
   const height = document.querySelector("#height").value;
   const weight = document.querySelector("#weight").value;
-  
+
   const res = await fetch(`/api?height=${height}&weight=${weight}`)
+  const data = await res.json()
 
-   const data = await res.json()
 
+  document.querySelector('#BMI-VALUE').textContent = data.bmi
+  document.querySelector("#BMI-CATEGORY").textContent = data.category
 
-document.querySelector('#BMI-VALUE').textContent = data.bmi
-document.querySelector("#BMI-CATEGORY").textContent = data.category
-  
 }
-
-
- 
